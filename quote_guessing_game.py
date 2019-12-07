b34e0893115c068432f8f238d88814b47188aba9
Harry Manchanda <harmanmanchanda182@gmail.com>
2019-12-07 15:01:51 +0530

    Fetching the data from all_quotes_scrapped csv file!

--- quote_guessing_game.py
+++ quote_guessing_game.py
@@ -1,34 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
-from csv import writer
 from random import choice
+from csv import DictReader
 
-BASE_URL = "http://quotes.toscrape.com"
 
-
-def scrape_quotes():
-    all_quotes_scrapped = []
-    page_url = "/page/1"
-    while page_url:
-        res = requests.get(f"{BASE_URL}{page_url}")
-        soup = BeautifulSoup(res.text, "html.parser")
-        all_quotes = soup.find_all(attrs={"class": "quote"})
-
-        for quote in all_quotes:
-            quote_author = quote.find(attrs={"class": "author"}).get_text()
-            quote_content = quote.find(attrs={"class": "text"}).get_text()
-            quote_author_slug = quote.find("a")["href"]
-            quote_author_link = f"{BASE_URL}{quote_author_slug}"
-
-            all_quotes_scrapped.append({
-                "quote_author": quote_author,
-                "quote_content": quote_content,
-                "quote_author_link": quote_author_link,
-            })
-
-        next_button = soup.find(attrs={"class": "next"})
-        page_url = next_button.find("a")["href"] if next_button else None
-    return all_quotes_scrapped
+def read_quotes(filename):
+    with open(filename, "r") as file:
+        csv_reader = DictReader(file)
+        return list(csv_reader)
 
 
 def start_game(quotes):
@@ -52,27 +31,8 @@ def start_game(quotes):
             break
 
         guesses_remaining -= 1
-
-        if guesses_remaining == 3:
-            res = requests.get(guess_quote_author_link)
-            soup = BeautifulSoup(res.text, "html.parser")
-            birth_date = soup.find(
-                attrs={"class": "author-born-date"}).get_text()
-            birth_place = soup.find(
-                attrs={"class": "author-born-location"}).get_text()
-            print(
-                f"Here is a Hint: The author was born on {birth_date} {birth_place}")
-        elif guesses_remaining == 2:
-            first_initial = guess_quote_author[0]
-            print(
-                f"Here is a Hint: The author first name starts with: {first_initial}")
-        elif guesses_remaining == 1:
-            last_initial = guess_quote_author.split(" ")[-1][0]
-            print(
-                f"Here is a Hint: The author last name starts with: {last_initial}")
-        else:
-            print(
-                f"Sorry, you ran out of guesses. The answer was {guess_quote_author}")
+        print_hint(guesses_remaining, guess_quote_author,
+                   guess_quote_author_link)
 
     while play_again.lower() not in ("y", "ye", "yes", "n", "no"):
         play_again = input("Would you like to play again (y/n)? ")
@@ -83,5 +43,28 @@ def start_game(quotes):
         print("Ohk, Goodbye")
 
 
-quotes = scrape_quotes()
+def print_hint(guesses_remaining, guess_quote_author, guess_quote_author_link):
+    if guesses_remaining == 3:
+        res = requests.get(guess_quote_author_link)
+        soup = BeautifulSoup(res.text, "html.parser")
+        birth_date = soup.find(
+            attrs={"class": "author-born-date"}).get_text()
+        birth_place = soup.find(
+            attrs={"class": "author-born-location"}).get_text()
+        print(
+            f"Here is a Hint: The author was born on {birth_date} {birth_place}")
+    elif guesses_remaining == 2:
+        first_initial = guess_quote_author[0]
+        print(
+            f"Here is a Hint: The author first name starts with: {first_initial}")
+    elif guesses_remaining == 1:
+        last_initial = guess_quote_author.split(" ")[-1][0]
+        print(
+            f"Here is a Hint: The author last name starts with: {last_initial}")
+    else:
+        print(
+            f"Sorry, you ran out of guesses. The answer was {guess_quote_author}")
+
+
+quotes = read_quotes("all_quotes_scrapped.csv")
 start_game(quotes)
--- quote_scrapping.py
+++ quote_scrapping.py
@@ -1,38 +1,48 @@
 import requests
 from bs4 import BeautifulSoup
 from csv import writer
+from random import choice
+from csv import DictWriter
 
-all_quotes_scrapped = []
+BASE_URL = "http://quotes.toscrape.com"
 
-base_url = "http://quotes.toscrape.com"
-page_url = "/page/1"
 
-while page_url:
-    res = requests.get(f"{base_url}{page_url}")
-    soup = BeautifulSoup(res.text, "html.parser")
-    all_quotes = soup.find_all(attrs={"class": "quote"})
+def scrape_quotes():
+    all_quotes_scrapped = []
+    page_url = "/page/1"
+    while page_url:
+        res = requests.get(f"{BASE_URL}{page_url}")
+        soup = BeautifulSoup(res.text, "html.parser")
+        all_quotes = soup.find_all(attrs={"class": "quote"})
 
-    for quote in all_quotes:
-        quote_author = quote.find(attrs={"class": "author"}).get_text()
-        quote_content = quote.find(attrs={"class": "text"}).get_text()
-        quote_author_slug = quote.find("a")["href"]
-        quote_author_link = f"{base_url}{quote_author_slug}"
+        for quote in all_quotes:
+            quote_author = quote.find(attrs={"class": "author"}).get_text()
+            quote_content = quote.find(attrs={"class": "text"}).get_text()
+            quote_author_slug = quote.find("a")["href"]
+            quote_author_link = f"{BASE_URL}{quote_author_slug}"
 
-        all_quotes_scrapped.append({
-            "quote_author": quote_author,
-            "quote_content": quote_content,
-            "quote_author_link": quote_author_link,
-        })
+            all_quotes_scrapped.append({
+                "quote_author": quote_author,
+                "quote_content": quote_content,
+                "quote_author_link": quote_author_link,
+            })
 
-    next_button = soup.find(attrs={"class": "next"})
-    page_url = next_button.find("a")["href"] if next_button else None
+        next_button = soup.find(attrs={"class": "next"})
+        page_url = next_button.find("a")["href"] if next_button else None
+    return all_quotes_scrapped
 
-with open("all_quotes_scrapped.csv", "w") as csv_file:
-    csv_writer = writer(csv_file)
-    csv_writer.writerow(["quote_author", "quote_content", "quote_author_link"])
 
-    for single_quote_scrapped in all_quotes_scrapped:
-        quote_author = single_quote_scrapped["quote_author"]
-        quote_content = single_quote_scrapped["quote_content"]
-        quote_author_link = single_quote_scrapped["quote_author_link"]
-        csv_writer.writerow([quote_author, quote_content, quote_author_link])
+def write_quotes(quotes):
+    with open("all_quotes_scrapped.csv", "w") as csv_file:
+        headers = ["quote_author", "quote_content", "quote_author_link"]
+        csv_writer = DictWriter(csv_file, fieldnames=headers)
+        csv_writer.writeheader()
+
+        for quote in quotes:
+            csv_writer.writerow(quote)
+
+        print("All quotes has been successfully scrapped")
+
+
+quotes = scrape_quotes()
+write_quotes(quotes)
